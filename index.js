a2d370abfc4bbde42765007e5f90d17fd0744ca2
Maneet Pal Singh <146899541+singhmaneet60@users.noreply.github.com>
2024-07-20 13:57:33 -0400

    updated again

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // Database configuration
 const config = {
     user: 'azureuser',
